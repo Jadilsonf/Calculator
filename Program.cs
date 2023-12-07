b654680fbf8bc3b2fdcc781596296d3099103578
Jadilsonf <jadilsonfilho171292@gmail.com>
2023-12-07 16:35:32 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+            var v1 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Segunda valor: ");
+            Double v2 = Double.Parse(Console.ReadLine());
+
+            Double resultado = v1 + v2;
+
+            Console.WriteLine("");
+            Console.WriteLine($"O resultado é {resultado}");
+
+        }
+    }
+}
