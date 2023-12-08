e42aa733e3bbe326109665cd730f36dce5810586
Jadilsonf <jadilsonfilho171292@gmail.com>
2023-12-08 14:06:11 -0300

    Finalizadas as operações

---
+++ Operations.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Operations
+        {
+
+        internal static void Soma()
+            {
+                Console.Clear();
+                Console.WriteLine("Primeiro valor: ");
+                var v1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Segunda valor: ");
+                Double v2 = Double.Parse(Console.ReadLine());
+
+                Double resultado = v1 + v2;
+
+                Console.WriteLine("");
+                Console.WriteLine($"O resultado da soma é {resultado}");
+                Console.ReadKey();
+            }
+
+        internal static void Subtracao()
+            {
+                Console.Clear();
+                Console.WriteLine("Primeiro valor: ");
+                var v1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Segunda valor: ");
+                Double v2 = Double.Parse(Console.ReadLine());
+
+                Double resultado = v1 - v2;
+
+                Console.WriteLine("");
+                Console.WriteLine($"O resultado da subtração é {resultado}");
+                Console.ReadKey();
+            }
+
+        internal static void Divisao()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+            var v1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Segunda valor: ");
+            Double v2 = Double.Parse(Console.ReadLine());
+
+            Double resultado = v1 / v2;
+
+            Console.WriteLine("");
+            Console.WriteLine($"O resultado da Divisão é {resultado}");
+            Console.ReadKey();
+        }
+
+        internal static void Multiplicacao()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+            var v1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Segunda valor: ");
+            Double v2 = Double.Parse(Console.ReadLine());
+
+            Double resultado = v1 * v2;
+
+            Console.WriteLine("");
+            Console.WriteLine($"O resultado da multiplicação é {resultado}");
+            Console.ReadKey();
+        }
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -6,18 +6,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.Clear();
-            Console.WriteLine("Primeiro valor: ");
-            var v1 = Convert.ToDouble(Console.ReadLine());
-            
-            Console.WriteLine("Segunda valor: ");
-            Double v2 = Double.Parse(Console.ReadLine());
-
-            Double resultado = v1 + v2;
-
-            Console.WriteLine("");
-            Console.WriteLine($"O resultado é {resultado}");
-
+            Operations.Multiplicacao();
         }
     }
-}
+ }
